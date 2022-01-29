092c966449ea7bf50fb35a00c5b87e87dc381558
Deivison Nogueira <deivison.nogueira@live.com>
2022-01-29 12:30:19 -0400

    cpf e email, tratando suas exceções

--- src/main/java/com/projetod/helpdesk/resourcesexceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/projetod/helpdesk/resourcesexceptions/ResourcesExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projetod.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.projetod.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @ControllerAdvice
@@ -23,6 +24,19 @@ public class ResourcesExceptionHandler {
 	    
 	    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	    }
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	 public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			 HttpServletRequest request){
+
+	    StandardError error = new StandardError(System.currentTimeMillis(),
+	    		HttpStatus.BAD_REQUEST.value(),"Violation" , ex.getMessage(),
+	    		request.getRequestURI());
+	    
+	    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	    }
+	
+	
 	}
 	
 
--- src/main/java/com/projetod/helpdesk/services/TecnicoService.java
+++ src/main/java/com/projetod/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projetod.helpdesk.domain.Pessoa;
 import com.projetod.helpdesk.domain.Tecnico;
 import com.projetod.helpdesk.domain.dto.TecnicoDTO;
+import com.projetod.helpdesk.repositories.PessoaRepository;
 import com.projetod.helpdesk.repositories.TecnicoRepository;
+import com.projetod.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.projetod.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
@@ -17,6 +20,8 @@ public class TecnicoService {
 	@Autowired
 	private TecnicoRepository repository;
 	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	
 	public Tecnico findById(Integer id) {
@@ -25,14 +30,31 @@ public class TecnicoService {
 	}
 
 
-
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
 	
+	
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validarCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
+
+
+	private void validarCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId())	{
+			throw new DataIntegrityViolationException("CPF ja cadastrado");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no Sistema");
+		}
+	}
+	
+	
+	
 }
